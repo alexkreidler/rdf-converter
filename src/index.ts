a12534f5f22d76c1e2ee7598f5a2dc5cb249f580
Alex Kreidler <alexkreidler2020@gmail.com>
2020-09-25 14:45:46 -0400

    refactor

--- src/index.ts
+++ src/index.ts
@@ -10,38 +10,42 @@ const mkdir = promises.mkdir
 
 import { basename } from "path"
 
+export async function convert(source: string, dest: string) {
+    console.log(`Converting from ${source} to ${dest}`)
+
+    if (source.includes("http")) {
+        throw new Error("We don't support remote resources yet")
+    }
+
+    const input = createReadStream(source)
+    const quadStream = rdfParse.parse(input, {
+        path: source,
+    })
+
+    mkdir(basename(dest), { recursive: true })
+
+    const outStream = createWriteStream(dest, "utf-8")
+    const outQuads = rdfSerialize.serialize(quadStream, { path: dest })
+
+    outQuads.pipe(outStream)
+
+    await new Promise((resolve, reject) => {
+        outQuads.on("end", () => {
+            resolve()
+        })
+        outQuads.on("error", () => {
+            reject()
+        })
+    })
+    console.log("Done!")
+}
+
 program
     .command("convert <source> <destination>")
-    .description("Converts between RDF serializations")
-    .action(async (source, dest) => {
-        console.log(`Converting from ${source} to ${dest}`)
-
-        if (source.includes("http")) {
-            throw new Error("We don't support remote resources yet")
-        }
-
-        const input = createReadStream(source)
-        const quadStream = rdfParse.parse(input, {
-            path: source,
-        })
-
-        mkdir(basename(dest), { recursive: true })
-
-        const outStream = createWriteStream(dest, "utf-8")
-        const outQuads = rdfSerialize.serialize(quadStream, { path: dest })
-
-        outQuads.pipe(outStream)
-
-        await new Promise((resolve, reject) => {
-            outQuads.on("end", () => {
-                resolve()
-            })
-            outQuads.on("error", () => {
-                reject()
-            })
-        })
-        console.log("Done!")
-    })
+    .description(
+        "Converts between RDF serializations. Needs file extensions to determine serialization type."
+    )
+    .action(convert)
 
 async function main() {
     try {
